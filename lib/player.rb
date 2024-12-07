930177bfaedaed104d0ece51817d419be0bce37a
Santi <sgcarvallo@gmail.com>
2024-12-06 23:00:08 -0600

    Switched things around with how the name and symbol are gotten from player

--- lib/player.rb
+++ lib/player.rb
@@ -6,15 +6,21 @@ class Player
   attr_accessor :name, :symbol
 
   # Player gets  initialized with name and symbol
-  def initialize # rubocop:disable Metrics/AbcSize
+  def initialize # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     # get name
     puts 'What is your name?'
     self.name = gets.chomp
-    puts 'Please enter a valid symbol' if name.to_s.empty? || name =~ /\A\s*\z/
+    while name.to_s.empty? || name =~ /\A\s*\z/
+      puts 'Please enter a valid name'
+      self.name = gets.chomp
+    end
     # get symbol
     puts 'What is your symbol?'
     self.symbol = gets.chomp
-    puts 'Please enter a valid symbol' if symbol.to_s.empty? || symbol =~ /\A\s*\z/
+    while symbol.to_s.empty? || symbol =~ /\A\s*\z/
+      puts 'Please enter a valid symbol'
+      self.symbol = gets.chomp
+    end
     # confirm player creation
     puts "#{name} has joined the game with the '#{symbol}' symbol"
   end
